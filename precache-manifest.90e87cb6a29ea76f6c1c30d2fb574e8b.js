d8dc00e59cc1b98cb26f92faf948fdaaec260ee2
USER <user@USERs-MacBook-Pro.local>
2020-03-05 04:41:26 -0500

    Updates

--- precache-manifest.90e87cb6a29ea76f6c1c30d2fb574e8b.js
+++ precache-manifest.90e87cb6a29ea76f6c1c30d2fb574e8b.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/Notes/static/js/runtime~main.7ba9aa76.js"
   },
   {
-    "revision": "2a435a6b8f531e89edba",
-    "url": "/Notes/static/js/main.bd89b825.chunk.js"
+    "revision": "239bfe7a4c4f64f287d1",
+    "url": "/Notes/static/js/main.70f402f4.chunk.js"
   },
   {
     "revision": "8b638ed3d5512c0831e6",
     "url": "/Notes/static/js/2.6f6f70bd.chunk.js"
   },
   {
-    "revision": "2a435a6b8f531e89edba",
+    "revision": "239bfe7a4c4f64f287d1",
     "url": "/Notes/static/css/main.7356266a.chunk.css"
   },
   {
-    "revision": "7f96ce618fdac1f0a68c94ea56e435c4",
+    "revision": "094b0f3782b8a0120407bb6b1d531b4b",
     "url": "/Notes/index.html"
   }
 ];
